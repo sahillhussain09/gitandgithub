79e5fc0b42f340e7d5d6730524e288f587f39b8b
sahillhussain09 <sh7389764@email.com>
2023-10-16 16:44:45 +0530

    Some nesessory changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import { Typography, Box } from '@mui/material'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
+  const [name, setName] = useState("");
+  const [nameVal, setNameVal] = useState("");
 
+  const handleName = () => {
+    if (nameVal.length > 0) {
+      setName(nameVal)
+    } else {
+      alert("please enter your name");
+    }
+  }
 
   return (
     <>
-      <h1>hello world</h1>
-      <p> author ~ sahil hussain
-        categary ~ MERN stack developer
-      </p>
+      <Box>
+        <Typography variant='h4'>Hello, {name ? name : ".................,"}</Typography>
+        <Box sx={{ margin: "10px" }}>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" size='small' onChange={(e) => setNameVal(e.target.value)} />
+          <Button color="secondary" placeholder='enter name' sx={{
+            marginLeft: "10px"
+          }} onClick={handleName}>Secondary</Button>
+        </Box>
+      </Box>
     </>
   )
 }
