d5b6c82d0b83e012ecd0fab806facaeac4784bef
sahillhussain09 <sh7389764@email.com>
2023-10-16 15:40:04 +0530

    Add React files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello world</h1>
+      <p> author ~ sahil hussain
+        categary ~ MERN stack developer
+      </p>
+    </>
+  )
+}
+
+export default App
